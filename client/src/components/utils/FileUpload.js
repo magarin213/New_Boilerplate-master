c53328ec9dc5860dde168425d3ebb06d794ce5c8
magarin <mario213@nate.com>
2021-03-16 03:28:46 +0900

    db에 이미지 보내기 성공

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,66 +1,76 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Dropzone from 'react-dropzone'
-import axios from 'axios'
 import { PlusOutlined } from '@ant-design/icons';
+import axios from 'axios';
 
-function FileUpload() {
 
+function FileUpload(props) {
 
     const [Images, setImages] = useState([])
 
     const dropHandler = (files) => {
 
         let formData = new FormData();
-
         const config = {
-            header : {'content-tryp': 'multipart/form-data'}
+            header: { 'content-type': 'multipart/fomr-data' }
         }
         formData.append("file", files[0])
 
-        axios.post('/api/product/image', formData, config )
-        .then(response => {
-            if(response.data.success){
-                setImages([...Images, response.data.filePath])
-            }else{
-                alert('파일을 저장하는데 실패했습니다.')
-            }
-        })
+        axios.post('/api/product/image', formData, config)
+            .then(response => {
+                if (response.data.success) {
+                    setImages([...Images, response.data.filePath])
+                    props.refreshFunction([...Images, response.data.filePath])
+
+
+                } else {
+                    alert('파일을 저장하는데 실패했습니다.')
+                }
+            })
     }
 
-    const delteHandler = (image) => {
-        const currentIndex = Images.indexOf(image)
-        
+
+    const deleteHandler = (image) => {
+        const currentIndex = Images.indexOf(image);
         let newImages = [...Images]
         newImages.splice(currentIndex, 1)
-
         setImages(newImages)
+        props.refreshFunction(newImages)
+
 
     }
 
 
     return (
-        <div style={{ display:'flex', justifyContent:'space-between'}}>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <Dropzone onDrop={dropHandler}>
-                {({getRootProps, getInputProps}) => (
-                    <div style={{width:300, height:240, border: '1px solid lightgray',
-                display:'flex', alignItems: 'center', justifyContent:'center'}}
-                    {...getRootProps()}>
+                {({ getRootProps, getInputProps }) => (
+                    <div
+                        style={{
+                            width: 300, height: 240, border: '1px solid lightgray',
+                            display: 'flex', alignItems: 'center', justifyContent: 'center'
+                        }}
+                        {...getRootProps()}>
                         <input {...getInputProps()} />
-                        <PlusOutlined type="plus" style={{fontSize: '3rem'}}/>
+                        <PlusOutlined type="plus" style={{ fontSize: '3rem' }} />
                     </div>
-
                 )}
             </Dropzone>
-                    <div style={{display:'flex', width: '350px', height:'240px', overflowX:'scroll'}}>
 
-                    {Images.map((image, index) =>(
-                        <div onClick={()=> delteHandler(image)} key={index}>
-                        <img style={{minWidth:'399px', width:'300px', height:'240px'}}
-                        src={`http://localhost:5000/${image}`}
+            <div style={{ display: 'flex', width: '350px', height: '240px', overflowX: 'scroll' }}>
+
+                {Images.map((image, index) => (
+                    <div onClick={() => deleteHandler(image)} key={index}>
+                        <img style={{ minWidth: '300px', width: '300px', height: '240px' }}
+                            src={`http://localhost:5000/${image}`}
                         />
                     </div>
-                    ))}
-                    </div>
+                ))}
+
+
+            </div>
+
+
         </div>
     )
 }
--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,21 +1,20 @@
-import React, {useState} from 'react'
-import {Typography, Button, Form, Input} from 'antd';
-import FileUpload from '../../utils/FileUpload'
-
-
-const {TextArea} = Input;
+import React, { useState } from 'react'
+import { Typography, Button, Form, Input } from 'antd';
+import FileUpload from '../../utils/FileUpload';
+import Axios from 'axios';
+const { TextArea } = Input;
 
 const Continents = [
-    {key:1, value:"Africa"},
-    {key:2, value:"Europe"},
-    {key:3, value:"Asia"},
-    {key:4, value:"North America"},
-    {key:5, value:"Sout America"},
-    {key:6, value:"Australia"},
-    {key:7, value:"Antarctica"}
+    { key: 1, value: "Africa" },
+    { key: 2, value: "Europe" },
+    { key: 3, value: "Asia" },
+    { key: 4, value: "North America" },
+    { key: 5, value: "South America" },
+    { key: 6, value: "Australia" },
+    { key: 7, value: "Antarctica" }
 ]
 
-function UploadProductPage() {
+function UploadProductPage(props) {
 
     const [Title, setTitle] = useState("")
     const [Description, setDescription] = useState("")
@@ -23,7 +22,7 @@ function UploadProductPage() {
     const [Continent, setContinent] = useState(1)
     const [Images, setImages] = useState([])
 
-    const titleChangHandler = (event) => {
+    const titleChangeHandler = (event) => {
         setTitle(event.currentTarget.value)
     }
 
@@ -39,44 +38,79 @@ function UploadProductPage() {
         setContinent(event.currentTarget.value)
     }
 
+    const updateImages = (newImages) => {
+        setImages(newImages)
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        if (!Title || !Description || !Price || !Continent || Images.length === 0) {
+            return alert(" 모든 값을 넣어주셔야 합니다.")
+        }
+
+
+        //서버에 채운 값들을 request로 보낸다.
+
+        const body = {
+            //로그인 된 사람의 ID 
+            writer: props.user.userData._id,
+            title: Title,
+            description: Description,
+            price: Price,
+            images: Images,
+            continents: Continent
+        }
+
+        Axios.post('/api/product', body)
+            .then(response => {
+                if (response.data.success) {
+                    alert('상품 업로드에 성공 했습니다.')
+                    props.history.push('/')
+                } else {
+                    alert('상품 업로드에 실패 했습니다.')
+                }
+            })
+    }
+
+
     return (
-        <div style={{ maxWidth: '700px', margin: '2rem auto'}}>
-            <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
-            <h2>여행 상품 업로드</h2>
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <h2> 여행 상품 업로드</h2>
             </div>
-            
-            <Form>
 
-        {/*dropzone*/}
+            <Form onSubmitCapture={submitHandler}>
+                {/* DropZone */}
+                <FileUpload refreshFunction={updateImages} />
 
-        <FileUpload/>
-        <br/>
-        <br/>
-        <label>이름</label>
-        <Input onChange={titleChangHandler} value={Title}/>
-        <br/>
-        <br/>
-        <label>설명</label>
-        <TextArea onChange={descriptionChangeHandler} value={Description} />
-        <br/>
-        <br/>
-        <label>가격($)</label>
-        <Input type="number" onChange={priceChangeHandler} value={Price}/>
-        <br/>
-        <br/>
-        <select onChange={continentChangeHandler} value={Continent}>
-            {Continents.map(item => (
-            <option key={item.key} value={item.key}>{item.value}</option>
-            ))}
-
-        </select>
-        <br/>
-        <br/>
-        <Button>
-            확인
-        </Button>
+                <br />
+                <br />
+                <label>이름</label>
+                <Input onChange={titleChangeHandler} value={Title} />
+                <br />
+                <br />
+                <label>설명</label>
+                <TextArea onChange={descriptionChangeHandler} value={Description} />
+                <br />
+                <br />
+                <label>가격($)</label>
+                <Input type="number" onChange={priceChangeHandler} value={Price} />
+                <br />
+                <br />
+                <select onChange={continentChangeHandler} value={Continent}>
+                    {Continents.map(item => (
+                        <option key={item.key} value={item.key}> {item.value}</option>
+                    ))}
+                </select>
+                <br />
+                <br />
+                <button type="submit">
+                    확인
+                </button>
             </Form>
-            
+
+
         </div>
     )
 }
