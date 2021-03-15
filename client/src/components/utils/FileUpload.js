6f782911785ae319be09ad01af8fc3109eef4864
magarin <mario213@nate.com>
2021-03-15 19:49:20 +0900

    이미지 업로드

---
+++ client/src/components/utils/FileUpload.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import Dropzone from 'react-dropzone'
+import axios from 'axios'
+import { PlusOutlined } from '@ant-design/icons';
+
+function FileUpload() {
+
+
+    const [Images, setImages] = useState([])
+
+    const dropHandler = (files) => {
+
+        let formData = new FormData();
+
+        const config = {
+            header : {'content-tryp': 'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+
+        axios.post('/api/product/image', formData, config )
+        .then(response => {
+            if(response.data.success){
+                setImages([...Images, response.data.filePath])
+            }else{
+                alert('파일을 저장하는데 실패했습니다.')
+            }
+        })
+    }
+
+    return (
+        <div style={{ display:'flex', justifyContent:'space-between'}}>
+            <Dropzone onDrop={dropHandler}>
+                {({getRootProps, getInputProps}) => (
+                    <div style={{width:300, height:240, border: '1px solid lightgray',
+                display:'flex', alignItems: 'center', justifyContent:'center'}}
+                    {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <PlusOutlined type="plus" style={{fontSize: '3rem'}}/>
+                    </div>
+
+                )}
+            </Dropzone>
+                    <div style={{display:'flex', width: '350px', height:'240px', overflowX:'scroll'}}>
+
+                    {Images.map((image, index) =>(
+                        <div key={index}>
+                        <img style={{minWidth:'399px', width:'300px', height:'240px'}}
+                        src={`http://localhost:5000/${image}`}
+                        />
+                    </div>
+                    ))}
+                    </div>
+        </div>
+    )
+}
+
+export default FileUpload
